b68245ece567ac1a2b053f14c7d7f917a6915dca
NathanScott85 <nathansfeely@gmail.com>
2018-05-20 18:21:49 +0100

    Added Ternary to Validation

--- src/Components/Validation.js
+++ src/Components/Validation.js
@@ -1,14 +1,14 @@
-import React from 'react';
+import React from "react";
 
-const validation = (props) => 
-{
-    return (
-        <div>
-            <p> Text too Short</p>
-        </div>
-    );
-
+const validation = props => {
+  return (
+    <div>
+      { props.inputLength > 5 ?
+      <p> Text too Long</p> :
+      <p> Text too Short</p>
+      }
+    </div>
+  );
 };
-
 
 export default validation;
